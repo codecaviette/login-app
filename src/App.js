eae3928d4630e422517aaa7329d8012c2c136f33
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-10-06 19:17:06 -0700

    Create form with Title and Email

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form className="login-form">
+      <h1 style={loginStyle} > Login Page </h1>    
+      <h2 className="text-center"> Welcome </h2>
+      <FormGroup>
+        <Label> Email </Label>
+        <Input type="email" placeholder="Email"/>
+      </FormGroup>
+    </Form>
   );
+}}
+
+//styling for Login Page header
+const loginStyle = {
+  fontStyle: 'bold',
+  textAlign: 'center',
+  padding: '10px'
 }
 
 export default App;
