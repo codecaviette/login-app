2520c96ddf3b839515f2c6baceebe3b331b2be0c
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-10-06 20:01:34 -0700

    Add columns and import image to App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Form, FormGroup, Label, Input, Container, Row, Media} from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col} from 'reactstrap';
 import { FacebookLoginButton } from 'react-social-login-buttons';
+import login from './login.jpeg';
 
 class App extends Component {
   render() {
   return (
     <Container>
       <Row>
-        <img 
-          src='./'
-          className="img-fluid"
-        />
-      </Row>
-      <Row>
-        <Form className="login-form">
-          <h1 style={loginStyle} > Login Page </h1>    
-          <h2 className="text-center"> Welcome </h2>
-          <FormGroup>
-            <Label> Email </Label>
-            <Input type="email" placeholder="Email"/>
-          </FormGroup>
-        </Form>
+        <Col>
+          <>
+          <img 
+            src={login}
+            className="img-fluid"
+            alt="generic placeholder"
+          />
+          </>
+        </Col>
+        <Col>
+          <Form className="login-form">
+            <h1 style={loginStyle} > Login Page </h1>    
+            <h2 className="text-center"> Welcome </h2>
+            <FormGroup>
+              <Label> Email </Label>
+              <Input type="email" placeholder="Email"/>
+            </FormGroup>
+          </Form>
+        </Col>
       </Row>
     </Container>
   );
